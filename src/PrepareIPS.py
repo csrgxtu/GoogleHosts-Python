5b9459c5ba306ccbe7e828a7dc7ff4d620817018
csrgxtu <1246506786@qq.com>
2015-03-30 11:38:26 +0800

    can list ips from blocks

--- src/PrepareIPS.py
+++ src/PrepareIPS.py
@@ -9,8 +9,24 @@
 from netaddr import *
 
 class PrepareIPS(object):
-  def __init__(self):
-    pass
+  InputSource = None
+
+  def __init__(self, source):
+    self.InputSource = source
 
   def run(self):
-    pass
+    IPBlocks = self.loadIPBlocks()
+
+    for block in IPBlocks:
+      ips = IPNetwork(block)
+      for ip in list(ips):
+        print str(ip)
+
+  def loadIPBlocks(self):
+    IPBlocks = []
+
+    with open(self.InputSource, 'r') as myFile:
+      for line in myFile:
+        IPBlocks.append(line.rstrip())
+
+    return IPBlocks
