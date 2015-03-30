0d9ba6e9eb4dc677453e1e0ee6bba1d95191f35a
csrgxtu <1246506786@qq.com>
2015-03-30 10:25:38 +0800

    structure of src/Connection.py

---
+++ src/Connection.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 30/Mar/2015
+# File: Connection.py
+# Desc: testing the connection to a host
+#
+# Produced By CSRGXTU
+
+class Connection(object):
+  host = None
+
+  def __init__(self, host):
+    pass
+
+  def httpConn(self):
+    pass
+
+  def httpsConn(self):
+    pass
+
+  def socketConn(self):
+    pass
+
+  def icmpConn(self):
+    pass
