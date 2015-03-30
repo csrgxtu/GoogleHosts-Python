3258b8666ad5f0f32a82a7da900def5d02d67701
csrgxtu <1246506786@qq.com>
2015-03-30 16:17:21 +0800

    test/multiprocessing_simple.py

---
+++ test/multiprocessing_simple.py
@@ -0,0 +1,13 @@
+import multiprocessing
+
+def worker():
+  """worker function"""
+  print "Worker"
+  return
+
+if __name__ == '__main__':
+  jobs = []
+  for i in range(5):
+    p = multiprocessing.Process(target=worker)
+    jobs.append(p)
+    p.start()
