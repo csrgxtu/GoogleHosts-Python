e6eebfbb15229bc347239b01ebdb96c18b032c2d
csrgxtu <1246506786@qq.com>
2015-03-30 12:04:41 +0800

    structure of main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 30/Mar/2015
+# File: main.py
+# Desc: the entrance file of this project
+#
+# Produced By CSRGXTU
+from Connection import Connection
+
+class main(object):
+  InputSource = None
+
+  def __init__(self, source):
+    self.InputSource = source
+    pass
+
+  def run(self):
+    pass
+
+m = main()
+m.run()
