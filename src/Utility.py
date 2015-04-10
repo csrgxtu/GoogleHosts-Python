df167447ce71ee0a127bff82c37915a88cf499ed
csrgxtu <1246506786@qq.com>
2015-04-10 09:02:26 +0800

    add appendLst2File to Utility.py

--- src/Utility.py
+++ src/Utility.py
@@ -44,3 +44,15 @@ def splitGenerator(lst, n):
   """ Yield successive n-sized chunks from lst."""
   for i in xrange(0, len(lst), n):
     yield lst[i:i+n]
+
+# append list 2 file
+#
+# @param outFile string
+# @param lst list
+# @return void
+def appendLst2File(outFile, lst):
+  with open(outFile, 'a') as myFile:
+    for item in lst:
+      myFile.write(item + '\n')
+
+    myFile.close()
