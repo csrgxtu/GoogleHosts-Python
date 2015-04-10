2aa964cba6c51065c155729ec78c587e403d89ff
csrgxtu <1246506786@qq.com>
2015-04-10 15:00:38 +0800

    temp commit

--- libs/SortIPs.py
+++ libs/SortIPs.py
@@ -7,15 +7,43 @@
 #
 # Produced By CSRGXTU
 from Connection import Connection
+from time import time
 
 class SortIPs(object):
   IPS = None
+  RES = None
 
   def __init__(self, ips):
-    pass
+    self.IPS = ips
+    self.RES = []
+  
+  # add connection times to the ips
+  #
+  # @return num int how many ips added times
+  def addTimes(self):
+    for ip in self.IPS:
+      start = time()
+      conn = Connection(ip)
+      if conn.httpsConn():
+        end = time()
+        self.RES.append([ip, end - start])
+
+    return len(self.RES)
 
   def asc(self):
     pass
 
   def desc(self):
+    pass
+
+  def getIPS(self):
+    pass
+
+  def getRES(self):
+    pass
+
+  def setIPS(self, ips):
+    pass
+
+  def setRES(self, lst):
     pass
