b1d23c5fe2aeecbe8c203aabaf853ac128e0e603
csrgxtu <1246506786@qq.com>
2015-04-10 14:39:08 +0800

    want to sort the ips

---
+++ src/TopIPS.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 10/Apr/2015
+# File: TopIPS.py
+# Desc: get top ips, most least connecting time
+#
+# Produced By CSRGXTU
+from Connection import Connection
+
+class TopIPS
