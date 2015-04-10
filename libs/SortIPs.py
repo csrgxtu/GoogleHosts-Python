e08cc128107e6e2c8aaf6bda4b085a3e59e2336c
csrgxtu <1246506786@qq.com>
2015-04-10 14:44:32 +0800

    structure of libs/SortIPs.py

---
+++ libs/SortIPs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 10/Apr/2015
+# File: SortIPs.py
+# Desc: sort ips according to the connection time
+#
+# Produced By CSRGXTU
+from Connection import Connection
+
+class SortIPs(object):
+  IPS = None
+
+  def __init__(self, ips):
+    pass
+
+  def asc(self):
+    pass
+
+  def desc(self):
+    pass
