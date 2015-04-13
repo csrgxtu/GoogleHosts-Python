eb4a4749a8bc9fb0ccaf6a61361bc8cae11b80b8
csrgxtu <1246506786@qq.com>
2015-04-13 13:28:35 +0800

    add libs/GenerateHosts.py structure

---
+++ libs/GenerateHosts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Author: Archer Reilly
+# Date: 13/Apr/2015
+# File: GenerateHosts.py
+# Desc: generate hosts file for Operating system
+#
+# Produced By CSRGXTU
+from SortIPs import SortIPs
+from Utility import *
+
+class GenerateHosts(object):
+  Input = None
+  Output = None
+
+  def __init__(self, Input, Output):
+    pass
+  
+  def run(self):
+    pass
+
+  def getInput(self):
+    pass
+
+  def getOutput(self):
+    pass
+
+  def setInput(self):
+    pass
+
+  def setOutput(self):
+    pass
